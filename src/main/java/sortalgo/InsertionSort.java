6fe93653dff687750cd46316b1315ed0551c3342
alena <alana.eremina@gmail.com>
2017-12-31 00:00:04 +0200

    insertion sort

--- src/main/java/sortalgo/InsertionSort.java
+++ src/main/java/sortalgo/InsertionSort.java
@@ -1,8 +1,19 @@
 package sortalgo;
 
-public class InsertionSort implements SortExample{
-    @Override
+public class InsertionSort implements SortExample {
+
     public int[] sort(int[] data) {
+        int temp;
+        for (int i = 1; i < data.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (data[j] < data[j - 1]) {
+                    temp = data[j];
+                    data[j] = data[j - 1];
+                    data[j - 1] = temp;
+                }
+            }
+
+        }
         return new int[0];
     }
 }
